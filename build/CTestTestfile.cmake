3530815c679dd308b46732d235623870281b881c
zeynelerenkinali <ernknal@gmail.com>
2024-07-31 15:30:43 +0100

    Updates

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eren/catkin_ws/src
+# Build directory: /home/eren/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot_control")
---
+++ build/turtlebot_control/CMakeFiles/basic_controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/eren/catkin_ws/devel/lib/turtlebot_control/basic_controller"
+  "/home/eren/catkin_ws/devel/lib/turtlebot_control/basic_controller.pdb"
+  "CMakeFiles/basic_controller.dir/src/basic_controller.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
